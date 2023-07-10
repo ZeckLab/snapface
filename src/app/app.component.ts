a546f4dd8fcff119f681ea7c0494f63ecb8f8204
ZeckLab <sturbout@gmail.com>
2023-07-10 16:13:02 +0200

    Utilisation de [ngStyle]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent implements OnInit{
         title: 'Paysage',
         description: 'Un paysage de montagne',
         createdDate: new Date(),
-        snaps: 0,
+        snaps: 100,
         imageUrl: "https://cdn.pixabay.com/photo/2017/02/01/22/02/mountain-landscape-2031539_1280.jpg",
         snapped: false,
         location: 'Alpes'
