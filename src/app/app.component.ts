17563a4a94e968aaca848677669adcf09c4a61e3
ZeckLab <sturbout@gmail.com>
2023-07-10 12:10:21 +0200

    Utilisation du *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,21 +7,20 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  myFirstFaceSnap!: FaceSnap;
-  mySecondFaceSnap!: FaceSnap;
-  myThirdFaceSnap!: FaceSnap;
+  faceSnapes!: FaceSnap[];
 
   ngOnInit(): void {
-    this.myFirstFaceSnap = new FaceSnap();
-    this.myFirstFaceSnap.title = 'Paysage';
-    this.myFirstFaceSnap.description = 'Un paysage de montagne';
-    this.myFirstFaceSnap.createdDate = new Date();
-    this.myFirstFaceSnap.snaps = 0;
-    this.myFirstFaceSnap.imageUrl = "https://cdn.pixabay.com/photo/2017/02/01/22/02/mountain-landscape-2031539_1280.jpg";
-    this.myFirstFaceSnap.snapped = false;
-    this.myFirstFaceSnap.location = 'Alpes';
-
-    this.mySecondFaceSnap = {
+    this.faceSnapes = [
+      {
+        title: 'Paysage',
+        description: 'Un paysage de montagne',
+        createdDate: new Date(),
+        snaps: 0,
+        imageUrl: "https://cdn.pixabay.com/photo/2017/02/01/22/02/mountain-landscape-2031539_1280.jpg",
+        snapped: false,
+        location: 'Alpes'
+      },
+      {
       title: 'Portrait',
       description: 'Un portrait de femme',
       createdDate: new Date(),
@@ -29,15 +28,41 @@ export class AppComponent implements OnInit{
       imageUrl: "https://cdn.pixabay.com/photo/2017/08/01/08/29/people-2563491_1280.jpg",
       snapped: false,
       location: 'Paris'
-    };
-
-    this.myThirdFaceSnap = {
-    title: 'Un oiseau',
-    description: 'Une mésange',
-    createdDate: new Date(),
-    snaps: 0,
-    imageUrl: "https://cdn.pixabay.com/photo/2023/07/01/11/40/old-tree-8100015_1280.jpg",
-    snapped: false
-    };
+      },
+      {
+      title: 'Un oiseau',
+      description: 'Une mésange',
+      createdDate: new Date(),
+      snaps: 0,
+      imageUrl: "https://cdn.pixabay.com/photo/2023/07/01/11/40/old-tree-8100015_1280.jpg",
+      snapped: false
+      },
+      {
+        title: 'Paysage',
+        description: 'Un paysage de montagne',
+        createdDate: new Date(),
+        snaps: 0,
+        imageUrl: "https://cdn.pixabay.com/photo/2017/02/01/22/02/mountain-landscape-2031539_1280.jpg",
+        snapped: false,
+        location: 'Alpes'
+      },
+      {
+      title: 'Portrait',
+      description: 'Un portrait de femme',
+      createdDate: new Date(),
+      snaps: 0,
+      imageUrl: "https://cdn.pixabay.com/photo/2017/08/01/08/29/people-2563491_1280.jpg",
+      snapped: false,
+      location: 'Paris'
+      },
+      {
+      title: 'Un oiseau',
+      description: 'Une mésange',
+      createdDate: new Date(),
+      snaps: 0,
+      imageUrl: "https://cdn.pixabay.com/photo/2023/07/01/11/40/old-tree-8100015_1280.jpg",
+      snapped: false
+      }
+    ];
   }
 }
