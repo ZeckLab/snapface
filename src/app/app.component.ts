3a3282011f6b931f6e2340b7425debaa91101875
ZeckLab <sturbout@gmail.com>
2023-07-14 15:20:51 +0200

    Ajout d'un header et ajout d'un service pour partager les données

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FaceSnap } from './models/face-snap.model';
 
 @Component({
   selector: 'app-root',
@@ -7,62 +6,5 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  faceSnapes!: FaceSnap[];
-
-  ngOnInit(): void {
-    this.faceSnapes = [
-      {
-        title: 'Paysage',
-        description: 'Un paysage de montagne',
-        createdDate: new Date(),
-        snaps: 100,
-        imageUrl: "https://cdn.pixabay.com/photo/2017/02/01/22/02/mountain-landscape-2031539_1280.jpg",
-        snapped: false,
-        location: 'Alpes'
-      },
-      {
-      title: 'Portrait',
-      description: 'Un portrait de femme',
-      createdDate: new Date(),
-      snaps: 0,
-      imageUrl: "https://cdn.pixabay.com/photo/2017/08/01/08/29/people-2563491_1280.jpg",
-      snapped: false,
-      location: 'Paris'
-      },
-      {
-      title: 'Un oiseau',
-      description: 'Une mésange',
-      createdDate: new Date(),
-      snaps: 0,
-      imageUrl: "https://cdn.pixabay.com/photo/2023/07/01/11/40/old-tree-8100015_1280.jpg",
-      snapped: false
-      },
-      {
-        title: 'Paysage',
-        description: 'Un paysage de montagne',
-        createdDate: new Date(),
-        snaps: 0,
-        imageUrl: "https://cdn.pixabay.com/photo/2017/02/01/22/02/mountain-landscape-2031539_1280.jpg",
-        snapped: false,
-        location: 'Alpes'
-      },
-      {
-      title: 'Portrait',
-      description: 'Un portrait de femme',
-      createdDate: new Date(),
-      snaps: 0,
-      imageUrl: "https://cdn.pixabay.com/photo/2017/08/01/08/29/people-2563491_1280.jpg",
-      snapped: false,
-      location: 'Paris'
-      },
-      {
-      title: 'Un oiseau',
-      description: 'Une mésange',
-      createdDate: new Date(),
-      snaps: 0,
-      imageUrl: "https://cdn.pixabay.com/photo/2023/07/01/11/40/old-tree-8100015_1280.jpg",
-      snapped: false
-      }
-    ];
-  }
+  ngOnInit(): void {}
 }
---
+++ src/app/services/face-snaps.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from "@angular/core";
+import { FaceSnap } from "../models/face-snap.model";
+
+@Injectable({
+    providedIn: "root"
+})
+export class FaceSnapsService {
+    faceSnapes: FaceSnap[] = [
+        {
+            title: 'Paysage',
+            description: 'Un paysage de montagne',
+            createdDate: new Date(),
+            snaps: 100,
+            imageUrl: "https://cdn.pixabay.com/photo/2017/02/01/22/02/mountain-landscape-2031539_1280.jpg",
+            snapped: false,
+            location: 'Alpes'
+        },
+        {
+            title: 'Portrait',
+            description: 'Un portrait de femme',
+            createdDate: new Date(),
+            snaps: 0,
+            imageUrl: "https://cdn.pixabay.com/photo/2017/08/01/08/29/people-2563491_1280.jpg",
+            snapped: false,
+            location: 'Paris'
+        },
+        {
+            title: 'Un oiseau',
+            description: 'Une mésange',
+            createdDate: new Date(),
+            snaps: 0,
+            imageUrl: "https://cdn.pixabay.com/photo/2023/07/01/11/40/old-tree-8100015_1280.jpg",
+            snapped: false
+        },
+        {
+            title: 'Paysage',
+            description: 'Un paysage de montagne',
+            createdDate: new Date(),
+            snaps: 0,
+            imageUrl: "https://cdn.pixabay.com/photo/2017/02/01/22/02/mountain-landscape-2031539_1280.jpg",
+            snapped: false,
+            location: 'Alpes'
+        },
+        {
+            title: 'Portrait',
+            description: 'Un portrait de femme',
+            createdDate: new Date(),
+            snaps: 0,
+            imageUrl: "https://cdn.pixabay.com/photo/2017/08/01/08/29/people-2563491_1280.jpg",
+            snapped: false,
+            location: 'Paris'
+        },
+        {
+            title: 'Un oiseau',
+            description: 'Une mésange',
+            createdDate: new Date(),
+            snaps: 0,
+            imageUrl: "https://cdn.pixabay.com/photo/2023/07/01/11/40/old-tree-8100015_1280.jpg",
+            snapped: false
+        }
+    ];
+}
